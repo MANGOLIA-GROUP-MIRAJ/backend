cc167ec767e65d9ecc3a868c7affd7eb251e3081
rajkamal8 <rajkamal.gupta.it23@heritageit.edu.in>
2021-06-01 20:28:48 +0530

    update

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
-
+const options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html'],
+    index: false,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+      res.set('x-timestamp', Date.now())
+    }
+};
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/public',express.static(path.join(__dirname, 'public')));
+// this is for public folder
+app.use(express.static("public",options));
 
 // routes
 app.get("/",function(req,res){
